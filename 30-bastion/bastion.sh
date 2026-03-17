996f08df88e96b0693841ecf1646fb9977f8f269
kranthi <kranthireddy152@gmail.com>
2026-03-17 08:24:20 +0530

    databases

--- 30-bastion/bastion.sh
+++ 30-bastion/bastion.sh
@@ -6,6 +6,7 @@ growpart /dev/nvme0n1 4
 lvextend -r -L +30G /dev/mapper/RootVG-homeVol
 xfs_growfs /home
 
+# terraform installation in bastion server
 yum install -y yum-utils
 yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 yum -y install terraform
